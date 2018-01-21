25c43a5528e060d45e1acdbd7f6ccaee11921a69
Yurio Miyazawa <me@yurio.net>
2018-01-20 16:05:23 -0800

    Update.

--- GatelessGateSharp/MainForm.cs
+++ GatelessGateSharp/MainForm.cs
@@ -2176,6 +2176,11 @@ namespace GatelessGateSharp {
                 InitializeChart(cartesianChartSpeedPrimaryAlgorithm, ConvertHashRateToString, double.NaN);
                 InitializeChart(cartesianChartSpeedSecondaryAlgorithm, ConvertHashRateToString, double.NaN);
                 InitializeChart(cartesianChartFanSpeed, value => value + "%", 100);
+
+            }
+            for (int i = 0; i < mDevices.Length; ++i) {
+                var color = ((System.Windows.Media.SolidColorBrush)(((LiveCharts.Wpf.LineSeries)cartesianChartTemperature.Series[i]).Stroke)).Color;
+                dataGridViewDevices.Rows[i].DefaultCellStyle.BackColor = Color.FromArgb(color.R, color.G, color.B);
             }
             UpdateChart(cartesianChartTemperature, "Temperature", deviceIndex => mDevices[deviceIndex].Temperature);
             UpdateChart(cartesianChartSpeedPrimaryAlgorithm,
